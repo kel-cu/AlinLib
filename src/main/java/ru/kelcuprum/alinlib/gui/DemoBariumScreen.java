226513af9dac49a4b56be34ca606a0c3612f7913
Kel Cuprum <kel@simplykel.ru>
2023-10-30 10:44:58 +0300

    Add scroll in Demo-Screen

--- src/main/java/ru/kelcuprum/alinlib/gui/DemoBariumScreen.java
+++ src/main/java/ru/kelcuprum/alinlib/gui/DemoBariumScreen.java
@@ -2,6 +2,7 @@ package ru.kelcuprum.alinlib.gui;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.AbstractScrollWidget;
 import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.screens.*;
 import net.minecraft.network.chat.Component;
@@ -20,60 +21,56 @@ public class DemoBariumScreen extends Screen {
     private static final Component CATEGORY = Component.literal("Example page");
     private static final Component EDIT_BOX = Component.literal("Edit Box");
     private static final Component EXIT = Component.literal("Exit");
+    //
+    private int scrolled = 0;
+    private int size = 900;
+    //
+    private EditBox editBox;
+    private BooleanButton booleanButton;
+    String[] hell = {
+            "Hello",
+            ",",
+            "World",
+            "!",
+            "No...",
+            "Welcome to Hell :)"
+    };
+    private SelectorStringButton selectorStringButton;
+        //
+
 
     public DemoBariumScreen(Screen parent) {
         super(CATEGORY);
         this.parent = parent;
     }
     public void tick() {
+        booleanButton.setY(40-scrolled);
+        editBox.setY(40+(25)-scrolled);
+        selectorStringButton.setY(40+(25*2)-scrolled);
         super.tick();
     }
 
     @Override
     public void init() {
+        scrolled = 0;
         initButton();
         initButtonsCategory();
     }
-    @Override
-    public void renderBackground(GuiGraphics guiGraphics, int i, int j, float f){
-        if(this.minecraft.level != null){
-            guiGraphics.fillGradient(0, 0, this.width, this.height, -1072689136, -804253680);
-        } else {
-            renderDirtBackground(guiGraphics);
-        }
-        guiGraphics.fill(0, 0, 130, this.height, new Color(0x80000000, true).getRGB());
-    }
 
-    public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTicks) {
-        guiGraphics.drawCenteredString(this.minecraft.font, this.title, width / 2 + 60, 15, -1);
-        guiGraphics.drawCenteredString(this.minecraft.font, TITLE, 120 / 2+5, 15, -1);
-        new TextBox(guiGraphics, 140, 40+25, this.font.width(EDIT_BOX)+16, 20, Component.literal("Edit Box"), false);
-        super.render(guiGraphics, mouseX, mouseY, partialTicks);
-
-    }
     private void initButtonsCategory(){
-
-        addRenderableWidget(new BooleanButton(140, 40, this.width - 150, 20, Component.literal("Boolean"), "Boolean", true, AlinLib.bariumConfig));
-
-
-        EditBox editBox = new EditBox(Minecraft.getInstance().font, 140+this.font.width(EDIT_BOX)+16, 40+(25), width-150-(this.font.width(EDIT_BOX)+16), 20, Component.literal("Hell"));
+//        addRenderableWidget(new (150, 0, 155, height, this.title));
+        booleanButton = new BooleanButton(140, 40, this.width - 150, 20, Component.literal("Boolean"), "Boolean", true, AlinLib.bariumConfig);
+        addRenderableWidget(booleanButton);
+        editBox = new EditBox(this.font, 140+this.font.width(EDIT_BOX)+16, 40+(25), width-150-(this.font.width(EDIT_BOX)+16), 20, Component.literal("Hell"));
         editBox.setValue(AlinLib.bariumConfig.getString("HELLO", "Hello, world!"));
         editBox.setResponder((string) -> {
-                    AlinLib.bariumConfig.setString("HELLO", string);
-                });
+            AlinLib.bariumConfig.setString("HELLO", string);
+        });
+
         addRenderableWidget(editBox);
         //
-        String[] hell = {
-                "Hello",
-                ",",
-                "World",
-                "!",
-                "No...",
-                "Welcome to Hell :)"
-        };
-        addRenderableWidget(
-                new SelectorStringButton(140, 40+(25*2), this.width - 150, 20, hell, AlinLib.bariumConfig, "selector", "Hello", Component.literal("Selector"))
-        );
+        selectorStringButton = new SelectorStringButton(140, 40+(25*2), this.width - 150, 20, hell, AlinLib.bariumConfig, "selector", hell[0], Component.literal("Selector"));
+        addRenderableWidget(selectorStringButton);
         //
     }
     private void initButton(){
@@ -86,5 +83,34 @@ public class DemoBariumScreen extends Screen {
             AlinLib.bariumConfig.save();
             this.minecraft.setScreen(parent);
         }));
+    }
+    @Override
+    public void renderBackground(GuiGraphics guiGraphics, int i, int j, float f){
+        if(this.minecraft.level != null){
+            guiGraphics.fillGradient(0, 0, this.width, this.height, -1072689136, -804253680);
+        } else {
+            renderDirtBackground(guiGraphics);
+        }
+        guiGraphics.fill(0, 0, 130, this.height, new Color(0x80000000, true).getRGB());
+    }
+
+    public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTicks) {
+        guiGraphics.drawCenteredString(this.minecraft.font, this.title, width / 2 + 60, 15-scrolled, -1);
+        guiGraphics.drawCenteredString(this.minecraft.font, TITLE, 120 / 2+5, 15, -1);
+        new TextBox(guiGraphics, 140, 40+25-scrolled, this.font.width(EDIT_BOX)+16, 20, Component.literal("Edit Box"), false);
+
+        guiGraphics.drawCenteredString(this.minecraft.font, this.title, width / 2 + 60, 875-scrolled, -1);
+        super.render(guiGraphics, mouseX, mouseY, partialTicks);
+
+    }
+
+    @Override
+    public boolean mouseScrolled(double d, double e, double f, double g) {
+        scrolled = (int) (scrolled + (g*10.0));
+        if(scrolled <= 0) scrolled = 0;
+        else if(scrolled >= size-height) scrolled = size-height;
+//        AlinLib.log("Scroll: "+ scrolled);
+
+        return super.mouseScrolled(d, e, f, g);
     }
 }
