0d6206c59fd21294c042bc321d5885c62ba65e1c
Kel <me@kelcuprum.ru>
2024-03-22 12:58:36 +0300

    1.2? - Replace demoClient to Module Demo - Re-fact AlinaToast - Another shit

--- Common/src/main/java/ru/kelcuprum/alinlib/gui/toast/AlinaToast.java
+++ Common/src/main/java/ru/kelcuprum/alinlib/gui/toast/AlinaToast.java
@@ -5,20 +5,20 @@ import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.PlayerFaceRenderer;
 import net.minecraft.client.gui.components.toasts.Toast;
 import net.minecraft.client.gui.components.toasts.ToastComponent;
-import net.minecraft.util.FormattedCharSequence;
-import net.minecraft.util.Mth;
 import ru.kelcuprum.alinlib.AlinLib;
 
 import org.jetbrains.annotations.NotNull;
-
-import java.util.Iterator;
-import java.util.List;
 
 public class AlinaToast implements Toast {
     private final ToastBuilder builder;
 
     protected AlinaToast(ToastBuilder builder) {
         this.builder = builder;
+    }
+
+    @Override
+    public int width() {
+        return Math.max(165, (builder.hasIcon() ? 38 : 16) + Math.max(AlinLib.MINECRAFT.font.width(this.builder.title), AlinLib.MINECRAFT.font.width(this.builder.message)));
     }
 
     @Override
@@ -29,48 +29,20 @@ public class AlinaToast implements Toast {
         guiGraphics.fill(0, 0, width(), height() - 1, colorBackground / 2 << 24);
         if (builder.type != ToastBuilder.Type.FLAT)
             guiGraphics.fill(0, height() - 1, width(), height(), builder.type.color);
+        else guiGraphics.fill(0, height() - 1, width(), height(), colorBackground / 2 << 24);
 
         Font font = AlinLib.MINECRAFT.font;
-
-        List<FormattedCharSequence> list = font.split(builder.message, 125);
-        int i = 16777215;
-        if (list.size() == 1) {
-            guiGraphics.drawString(font, builder.title, builder.hasIcon() ? 30 : 8, builder.hasIcon() ? 7 : 8, i, false);
-            guiGraphics.drawString(font, list.get(0), builder.hasIcon() ? 30 : 8, builder.hasIcon() ? 18 : 19, i, false);
-        } else {
-            float f = 300.0F;
-            int k;
-            if (l < 1000L) {
-                k = Mth.floor(Mth.clamp((float) (1000L - l) / f, 0.0F, 1.0F) * 255.0F) << 24 | 67108864;
-                guiGraphics.drawString(font, builder.title, builder.hasIcon() ? 30 : 8, builder.hasIcon() ? 11 : 8, i | k, false);
-            } else {
-                k = Mth.floor(Mth.clamp((float) (l - 1000L) / f, 0.0F, 1.0F) * 252.0F) << 24 | 67108864;
-                int var10001 = list.size();
-                int m = (this.height() / 2) - var10001 * 9 / 2;
-
-                for (Iterator<FormattedCharSequence> var12 = list.iterator(); var12.hasNext(); m += 9) {
-                    guiGraphics.drawString(font, var12.next(), builder.hasIcon() ? 30 : 8, m, i | k, false);
-                }
-            }
-        }
-
+        guiGraphics.drawString(font, builder.title, builder.hasIcon() ? 30 : 8, builder.hasIcon() ? 7 : 8, -1, false);
+        guiGraphics.drawString(font, builder.message, builder.hasIcon() ? 30 : 8, builder.hasIcon() ? 18 : 19, -1, false);
         if (builder.hasIcon()) {
-            if (builder.icon != null) {
-                guiGraphics.blit(builder.icon, 8, 8, 0.0F, 0.0F, 16, 16, 16, 16);
-
-            } else if (builder.itemIcon != null) {
-                guiGraphics.renderFakeItem(builder.itemIcon, 8, 8);
-
-            } else if (builder.playerIcon != null) {
-                PlayerFaceRenderer.draw(guiGraphics, builder.playerIcon, 8, 8, 16);
-            }
+            if (builder.icon != null) guiGraphics.blit(builder.icon, 8, 8, 0.0F, 0.0F, 16, 16, 16, 16);
+            else if (builder.itemIcon != null) guiGraphics.renderFakeItem(builder.itemIcon, 8, 8);
+            else if (builder.playerIcon != null) PlayerFaceRenderer.draw(guiGraphics, builder.playerIcon, 8, 8, 16);
         }
 
         Visibility visibility = (double) l >= builder.displayTime * toastComponent.getNotificationDisplayTimeMultiplier() ? Visibility.HIDE : Visibility.SHOW;
 
-        if (builder.visibilityVisitor != null) {
-            visibility = builder.visibilityVisitor.apply(visibility);
-        }
+        if (builder.visibilityVisitor != null) visibility = builder.visibilityVisitor.apply(visibility);
 
         return visibility;
     }
