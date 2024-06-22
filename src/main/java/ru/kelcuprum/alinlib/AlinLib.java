bbc45f7efd0b69a75d0fb8a6767acde9244daad7
Kel <me@kelcuprum.ru>
2024-06-22 22:14:07 +0300

    АХУЙ

---
+++ src/main/java/ru/kelcuprum/alinlib/AlinLib.java
@@ -0,0 +1,264 @@
+package ru.kelcuprum.alinlib;
+
+import meteordevelopment.starscript.value.ValueMap;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.Items;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.lwjgl.glfw.GLFW;
+import ru.kelcuprum.alinlib.api.KeyMappingHelper;
+import ru.kelcuprum.alinlib.api.events.alinlib.AlinLibEvents;
+import ru.kelcuprum.alinlib.api.events.client.ClientLifecycleEvents;
+import ru.kelcuprum.alinlib.api.events.alinlib.LocalizationEvents;
+import ru.kelcuprum.alinlib.api.events.client.ClientTickEvents;
+import ru.kelcuprum.alinlib.api.events.client.GuiRenderEvents;
+import ru.kelcuprum.alinlib.config.Config;
+import ru.kelcuprum.alinlib.config.Localization;
+import ru.kelcuprum.alinlib.config.parser.StarScript;
+import ru.kelcuprum.alinlib.config.parser.info.Player;
+import ru.kelcuprum.alinlib.gui.GuiUtils;
+import ru.kelcuprum.alinlib.gui.config.ConfigScreen;
+import ru.kelcuprum.alinlib.gui.styles.FlatStyle;
+import ru.kelcuprum.alinlib.gui.styles.ModernStyle;
+import ru.kelcuprum.alinlib.gui.toast.ToastBuilder;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Random;
+
+import static ru.kelcuprum.alinlib.gui.Icons.CLOWNFISH;
+//#if FORGE
+//$$ @net.minecraftforge.fml.common.Mod("alinlib")
+//#elseif NEOFORGE
+//$$ @net.neoforged.fml.common.Mod("alinlib")
+//#endif
+public class AlinLib
+    //#if FABRIC
+        implements net.fabricmc.api.ClientModInitializer
+    //#endif
+{
+    public static boolean isFabricLoader = false;
+    public static final String MODID = "alinlib";
+    public static String VERSION = "alinlib";
+    public static final Logger LOG = LogManager.getLogger("AlinaLib");
+    public static Config bariumConfig = new Config("config/AlinLib/config.json");
+    public static Localization localization = new Localization("alinlib","config/AlinLib/lang");
+    public static Minecraft MINECRAFT = Minecraft.getInstance();
+    public static StarScript starScript;
+    public static HashMap<String, Double> funnyCoordinatesX$alinLib = new HashMap<>();
+    public static HashMap<String, Double> funnyCoordinatesZ$alinLib = new HashMap<>();
+    ///
+    public static HashMap<String, Double> funnyCoordinatesX$imGRUI = new HashMap<>();
+    public static HashMap<String, Double> funnyCoordinatesZ$imGRUI = new HashMap<>();
+
+    // Init
+    public static void init() {
+        starScript = new StarScript();
+        GuiUtils.registerStyle(new FlatStyle());
+        GuiUtils.registerStyle(new ModernStyle());
+        KeyMapping toggleStealth = KeyMappingHelper.register(new KeyMapping(
+                "alinlib.key.stealth",
+                GLFW.GLFW_KEY_UNKNOWN,
+                "alinlib"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            assert client.player != null;
+            while (toggleStealth.consumeClick()) {
+                bariumConfig.setBoolean("STREAMER.STEALTH", !bariumConfig.getBoolean("STREAMER.STEALTH", false));
+                bariumConfig.save();
+            }
+        });
+
+        ClientLifecycleEvents.CLIENT_STARTED.register((client) -> {
+            LocalizationEvents.DEFAULT_PARSER_INIT.invoker().onParserInit(starScript);
+            AlinLib.log(String.format("Client started. MC Version: %s", client.getLaunchedVersion()));
+        });
+        ClientLifecycleEvents.CLIENT_FULL_STARTED.register((client) -> {
+            AlinLib.log(String.format("Client full started. MC Version: %s", client.getLaunchedVersion()));
+            isAprilFool();
+            isHBKel();
+        });
+        ClientLifecycleEvents.CLIENT_STOPPING.register((client) -> {
+            log("This world goes round and round like a carousel in a circus.");
+            log("Maybe the world is a circus?)");
+        });
+        LocalizationEvents.DEFAULT_PARSER_INIT.register(parser -> parser.ss.set("alinlib", new ValueMap()
+                .set("id", MODID)
+                .set("version", VERSION))
+        );
+        AlinLibEvents.INIT.invoker().onInit();
+    }
+    //#if FABRIC
+    @Override
+    public void onInitializeClient() {
+        AlinLib.isFabricLoader = true;
+        AlinLib.VERSION = net.fabricmc.loader.api.FabricLoader.getInstance().getModContainer(AlinLib.MODID).get().getMetadata().getVersion().getFriendlyString();
+        KeyMappingHelper.onRegister = net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper::registerKeyBinding;
+        init();
+    }
+    //#elseif FORGE
+    //$$  public static final java.util.List<KeyMapping> EXAMPLE_MAPPING = new java.util.ArrayList<>();
+    //$$  public AlinLib(){
+    //$$      KeyMappingHelper.onRegister = (s) -> {
+    //$$          EXAMPLE_MAPPING.add(s);
+    //$$          return s;
+    //$$      };
+    //$$      init();
+    //$$     for(net.minecraftforge.forgespi.language.IModInfo mod : net.minecraftforge.fml.ModList.get().getMods()){
+    //$$          if(mod.getModId().equals(AlinLib.MODID)){
+    //$$              AlinLib.VERSION = mod.getVersion().getQualifier();
+    //$$          }
+    //$$      }
+    //$$      if (net.minecraftforge.fml.loading.FMLLoader.getDist() == net.minecraftforge.api.distmarker.Dist.CLIENT) {
+    //$$          registerScreen();
+    //$$          net.minecraftforge.common.MinecraftForge.EVENT_BUS.addListener(this::registerBindings);
+    //$$          net.minecraftforge.common.MinecraftForge.EVENT_BUS.addListener(this::onPostRenderGui);
+    //$$      }
+    //$$  }
+    //$$  public void onPostRenderGui(net.minecraftforge.client.event.RenderGuiEvent.Post event) {
+    //$$      GuiRenderEvents.RENDER.invoker().onRender(event.getGuiGraphics(), event.getPartialTick());
+    //$$  }
+    //$$  @net.minecraftforge.eventbus.api.SubscribeEvent
+    //$$  public void registerBindings(net.minecraftforge.client.event.RegisterKeyMappingsEvent event) {
+    //$$      for(KeyMapping mapping : EXAMPLE_MAPPING) {
+    //$$          event.register(mapping);
+    //$$      }
+    //$$  }
+    //#elseif NEOFORGE
+    //$$  public static final java.util.List<KeyMapping> EXAMPLE_MAPPING = new java.util.ArrayList<>();
+    //$$  public AlinLib(){
+    //$$      KeyMappingHelper.onRegister = (s) -> {
+    //$$          EXAMPLE_MAPPING.add(s);
+    //$$          return s;
+    //$$      };
+    //$$      init();
+    //$$      for(net.neoforged.neoforgespi.language.IModInfo mod : net.neoforged.fml.ModList.get().getMods()){
+    //$$          if(mod.getModId().equals(AlinLib.MODID)){
+    //$$              AlinLib.VERSION = mod.getVersion().getQualifier();
+    //$$          }
+    //$$      }
+    //$$      if (net.neoforged.fml.loading.FMLLoader.getDist() == net.neoforged.api.distmarker.Dist.CLIENT) {
+    //$$          net.neoforged.fml.ModLoadingContext.get().registerExtensionPoint(
+    //$$                  net.neoforged.neoforge.client.gui.IConfigScreenFactory.class,
+    //$$                  () -> (minecraftClient, screen) -> ConfigScreen.build(screen));
+    //$$          final net.neoforged.bus.api.IEventBus bus = net.neoforged.fml.ModLoadingContext.get().getActiveContainer().getEventBus();
+    //$$          bus.addListener(this::registerBindings);
+    //$$          net.neoforged.neoforge.common.NeoForge.EVENT_BUS.addListener(this::onPostRenderGui);
+    //$$      }
+    //$$  }
+    //$$  @net.neoforged.bus.api.SubscribeEvent
+    //$$  public void registerBindings(net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent event) {
+    //$$      for(KeyMapping mapping : EXAMPLE_MAPPING) {
+    //$$          event.register(mapping);
+    //$$      }
+    //$$  }
+    //#endif
+
+    //#if NEOFORGE && MC >= 12100
+    //$$  public void onPostRenderGui(net.neoforged.neoforge.client.event.RenderGuiEvent.Post event) {
+    //$$      GuiRenderEvents.RENDER.invoker().onRender(event.getGuiGraphics(), event.getPartialTick().getGameTimeDeltaTicks());
+    //$$  }
+    //#elseif NEOFORGE && MC < 12100
+    //$$  public void onPostRenderGui(net.neoforged.neoforge.client.event.RenderGuiEvent.Post event) {
+    //$$      GuiRenderEvents.RENDER.invoker().onRender(event.getGuiGraphics(), event.getPartialTick());
+    //$$  }
+    //#endif
+
+    //#if FORGE && MC < 12002
+    //$$ public void registerScreen(){
+    //$$          net.minecraftforge.fml.ModLoadingContext.get().registerExtensionPoint(
+    //$$                  net.minecraftforge.client.ConfigScreenHandler.ConfigScreenFactory.class,
+    //$$                  () -> new net.minecraftforge.client.ConfigScreenHandler.ConfigScreenFactory((java.util.function.Function<net.minecraft.client.gui.screens.Screen, net.minecraft.client.gui.screens.Screen>) ConfigScreen::build));
+    //$$ }
+    //#elseif FORGE && MC >= 12002
+    //$$ public void registerScreen(){
+    //$$          net.minecraftforge.fml.ModLoadingContext.get().registerExtensionPoint(
+    //$$                  net.minecraftforge.client.ConfigScreenHandler.ConfigScreenFactory.class,
+    //$$                  () -> new net.minecraftforge.client.ConfigScreenHandler.ConfigScreenFactory(ConfigScreen::build));
+    //$$ }
+    //#endif
+    // Stealth
+    public static double getFunnyValueCoordinate(double coordinate, String server, String world, boolean isX){
+        return switch (bariumConfig.getNumber("STREAMER.STEALTH.TYPE", 0).intValue()){
+            case 1 -> getFunnyValueCoordinate$kelVersion(coordinate, server, world, isX);
+            default -> getFunnyValueCoordinate$ImGRUIVersion(coordinate, server, world, isX);
+        };
+    }
+    public static double getFunnyValueCoordinate$kelVersion(double coordinate, String server, String world, boolean isX){
+        String info =  server + "-" + world;
+        double value;
+        if(isX ? funnyCoordinatesX$alinLib.containsKey(info) : funnyCoordinatesZ$alinLib.containsKey(info)) value = isX ? funnyCoordinatesX$alinLib.get(info) : funnyCoordinatesZ$alinLib.get(info);
+        else {
+            while(true){
+                double r = Math.random();
+                int i = Math.random() < 0.5 ? -1 : 1;
+                double m= Math.random() * 10;
+                value = r*i*m;
+                if((value > -1.25 && value < -0.75 ) || (value > 0.75 && value < 1.25)){
+                    if(isX) funnyCoordinatesX$alinLib.put(info, value); else funnyCoordinatesZ$alinLib.put(info, value);
+                    log(info+": "+value+(isX ? " x" : " z"));
+                    break;
+                }
+            }
+        }
+        return coordinate * value;
+    }
+    public static double getFunnyValueCoordinate$ImGRUIVersion(double coordinate, String server, String world, boolean isX){
+        String info =  server + "-" + world;
+        double value;
+        if(isX ? funnyCoordinatesX$imGRUI.containsKey(info) : funnyCoordinatesZ$imGRUI.containsKey(info)) value = isX ? funnyCoordinatesX$imGRUI.get(info) : funnyCoordinatesZ$imGRUI.get(info);
+        else {
+            while(true){
+                int i = Math.random() < 0.5 ? -1 : 1;
+                value = 2*Math.random()*i;
+                if((value > -1.25 && value < -0.75 ) || (value > 0.75 && value < 1.25)){
+                    if(isX) funnyCoordinatesX$imGRUI.put(info, value); else funnyCoordinatesZ$imGRUI.put(info, value);
+                    log(info+": "+value+(isX ? " x" : " z"));
+                    break;
+                }
+            }
+        }
+        if(!isX) {
+            if (Player.getX() > 0 && coordinate > 0 && value > 0) value *= -1;
+        }
+        return coordinate*value;
+    }
+    // Funny
+    public static void isAprilFool(){
+        if(LocalDate.now().getMonthValue() == 4 && LocalDate.now().getDayOfMonth() == 1){
+            String[] types = {
+                    "white",
+                    "welcome",
+                    "clownfish"
+            };
+            String type = types[new Random().nextInt(types.length)];
+            new ToastBuilder()
+                    .setIcon(CLOWNFISH)
+                    .setTitle(Component.literal("AlinLib"))
+                    .setMessage(Component.translatable("alinlib.april_fools."+type))
+                    .setType(ToastBuilder.Type.WARN)
+                    .show(MINECRAFT.getToasts());
+        }
+    }
+    public static void isHBKel(){
+        if(LocalDate.now().getMonthValue() == 4 && LocalDate.now().getDayOfMonth() == 8){
+            if(bariumConfig.getBoolean("KEL_HB_"+LocalDate.now().getYear(), false)) return;
+            bariumConfig.setBoolean("KEL_HB_"+LocalDate.now().getYear(), true);
+            new ToastBuilder()
+                    .setIcon(Items.CAKE)
+                    .setTitle(Component.literal("AlinLib"))
+                    .setMessage(Component.translatable("alinlib.hb"))
+                    .show(MINECRAFT.getToasts());
+        }
+    }
+
+    // Logs
+    public static void log(Component message) { log(message, Level.INFO);}
+    public static void log(Component message, Level level) { log(message.getString(), level);}
+    public static void log(String message) { log(message, Level.INFO);}
+    public static void log(String message, Level level) { LOG.log(level, "[" + LOG.getName() + "] " + message); }
+}
---
+++ src/main/java/ru/kelcuprum/alinlib/gui/screens/AbstractConfigScreen.java
@@ -0,0 +1,267 @@
+package ru.kelcuprum.alinlib.gui.screens;
+
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.AbstractWidget;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.CommonComponents;
+import net.minecraft.network.chat.Component;
+import org.jetbrains.annotations.NotNull;
+import org.lwjgl.glfw.GLFW;
+import ru.kelcuprum.alinlib.AlinLib;
+import ru.kelcuprum.alinlib.config.Localization;
+import ru.kelcuprum.alinlib.gui.Colors;
+import ru.kelcuprum.alinlib.gui.components.ConfigureScrolWidget;
+import ru.kelcuprum.alinlib.gui.components.Description;
+import ru.kelcuprum.alinlib.gui.components.Resetable;
+import ru.kelcuprum.alinlib.gui.components.buttons.base.Button;
+import ru.kelcuprum.alinlib.gui.components.buttons.ButtonSprite;
+import ru.kelcuprum.alinlib.gui.components.text.DescriptionBox;
+import ru.kelcuprum.alinlib.gui.components.text.TextBox;
+import ru.kelcuprum.alinlib.gui.toast.ToastBuilder;
+
+import java.util.List;
+
+import static ru.kelcuprum.alinlib.gui.Icons.RESET;
+
+public class AbstractConfigScreen extends Screen {
+
+    private ConfigureScrolWidget scroller;
+    private ConfigureScrolWidget scroller_panel;
+    private final ConfigScreenBuilder builder;
+    private DescriptionBox descriptionBox;
+    private boolean lastCheck = false;
+    public AbstractConfigScreen(ConfigScreenBuilder builder) {
+        super(builder.title);
+        this.builder = builder;
+    }
+
+    @Override
+    protected void init() {
+        initPanelButtons();
+        initCategory();
+        assert this.minecraft != null;
+        if (this.minecraft.getLastInputType().isKeyboard()) {
+            if (!this.builder.panelWidgets.isEmpty()) this.setFocused(getFirstActiveWidget(this.builder.panelWidgets));
+            else if (!this.builder.widgets.isEmpty()) this.setFocused(getFirstActiveWidget(this.builder.widgets));
+            else this.setFocused(back);
+        }
+    }
+    protected AbstractWidget getFirstActiveWidget(List<AbstractWidget> widgets){
+        AbstractWidget widget = widgets.get(0);
+        for(AbstractWidget abstractWidget : widgets){
+            if(abstractWidget.isActive()) {
+                widget = abstractWidget;
+                break;
+            }
+        }
+        return widget;
+    }
+    AbstractWidget titleW;
+    AbstractWidget back;
+    AbstractWidget reset;
+    protected void initPanelButtons(){
+        // -=-=-=-=-=-=-=-
+        titleW = addRenderableWidget(new TextBox(10, 5, this.builder.panelSize-20, 30, this.builder.title, true));
+        // -=-=-=-=-=-=-=-
+        this.descriptionBox = new DescriptionBox(10, 40, this.builder.panelSize-20, height - 75, Component.empty());
+        this.descriptionBox.visible = false;
+        addRenderableWidget(this.descriptionBox);
+        // -=-=-=-=-=-=-=-
+        // Exit Buttons
+        // 85 before reset button
+        back = addRenderableWidget(new Button(10, height - 30, this.builder.panelSize-45, 20, builder.style, CommonComponents.GUI_BACK, (OnPress) -> {
+            assert this.minecraft != null;
+            this.minecraft.setScreen(builder.parent);
+        }));
+        reset = addRenderableWidget(new ButtonSprite(this.builder.panelSize-30, height-30, 20, 20, builder.style, RESET, Localization.getText("alinlib.component.reset"), (OnPress) -> {
+            for(AbstractWidget widget : builder.widgets){
+                if(widget instanceof Resetable){
+                    ((Resetable) widget).resetValue();
+                }
+            }
+            assert this.minecraft != null;
+            new ToastBuilder()
+                    .setTitle(title)
+                    .setMessage(Component.translatable("alinlib.component.reset.toast"))
+                    .setIcon(RESET)
+                    .show(this.minecraft.getToasts());
+            AlinLib.log(Component.translatable("alinlib.component.reset.toast"));
+        }));
+        this.scroller_panel = addRenderableWidget(new ConfigureScrolWidget(-8, 0, 4, this.height, Component.empty(), scroller -> {
+            scroller.innerHeight = 5;
+            if(lastCheck){
+                titleW.setY(5);
+                back.setY(height-25);
+                reset.setY(height-25);
+            } else {
+                titleW.setY((int) (scroller.innerHeight - scroller.scrollAmount()));
+                scroller.innerHeight+=titleW.getHeight()+5;
+
+                for(AbstractWidget widget : builder.panelWidgets){
+                    if(widget.visible){
+                        widget.setY((int) (scroller.innerHeight - scroller.scrollAmount()));
+                        scroller.innerHeight += (widget.getHeight()+5);
+                    } else widget.setY(-widget.getHeight());
+                }
+                if(scroller.innerHeight >= height-25){
+                    back.setY((int) (scroller.innerHeight - scroller.scrollAmount()));
+                    reset.setY((int) (scroller.innerHeight - scroller.scrollAmount()));
+                    scroller.innerHeight += (20);
+                }
+            }
+        }));
+        addRenderableWidgets(builder.panelWidgets);
+    }
+    protected void initCategory(){
+        int width = this.width - this.builder.panelSize - 20;
+        for(AbstractWidget widget : builder.widgets){
+            widget.setWidth(width);
+            widget.setX(this.builder.panelSize+10);
+        }
+        this.scroller = addRenderableWidget(new ConfigureScrolWidget(this.width - 8, 0, 4, this.height, Component.empty(), scroller -> {
+            scroller.innerHeight = 5;
+            boolean descriptionEnable = false;
+            for(AbstractWidget widget : builder.widgets){
+                if(widget.visible){
+                    if(widget instanceof Description){
+                        if(widget.isHoveredOrFocused() && ((Description) widget).getDescription() != null && this.descriptionBox != null){
+                            descriptionEnable = true;
+                            this.descriptionBox.setDescription(((Description) widget).getDescription());
+                        }
+                    }
+                    widget.setY((int) (scroller.innerHeight - scroller.scrollAmount()));
+                    scroller.innerHeight += (widget.getHeight()+5);
+                } else widget.setY(-widget.getHeight());
+            }
+            if(this.lastCheck != descriptionEnable){
+                lastCheck = descriptionEnable;
+                for(AbstractWidget widget : builder.panelWidgets){
+                    widget.visible = !lastCheck;
+                }
+                this.descriptionBox.visible = lastCheck;
+            }
+        }));
+        addRenderableWidgets(builder.widgets);
+    }
+
+    public void removeWidgetFromBuilder(){
+        for (AbstractWidget widget : this.builder.widgets) {
+            removeWidget(widget);
+        }
+        this.builder.widgets.clear();
+    }
+    public void removePanelWidgetFromBuilder(){
+        for (AbstractWidget widget : this.builder.panelWidgets) {
+            removeWidget(widget);
+        }
+        this.builder.panelWidgets.clear();
+    }
+
+    public void rebuildPanel(){
+        removeWidget(scroller_panel);
+        scroller_panel = null;
+        removeWidget(titleW);
+        removeWidget(back);
+        removeWidget(reset);
+        removeWidget(descriptionBox);
+        for (AbstractWidget widget : this.builder.panelWidgets) {
+            removeWidget(widget);
+        }
+        initPanelButtons();
+    }
+    public void rebuildCategory(){
+        removeWidget(scroller);
+        scroller = null;
+        for (AbstractWidget widget : this.builder.widgets) {
+            removeWidget(widget);
+        }
+        initCategory();
+    }
+
+    // Добавление виджетов
+    protected void addRenderableWidgets(@NotNull List<AbstractWidget> widgets){
+        for(AbstractWidget widget : widgets){
+            addRenderableWidget(widget);
+        }
+    }
+
+    // Рендер, скролл, прослушивание кей-биндов
+    @Override
+    public void tick(){
+        if(scroller != null) scroller.onScroll.accept(scroller);
+        if(scroller_panel != null) scroller_panel.onScroll.accept(scroller_panel);
+        if(builder.onTick != null) builder.onTick.onTick(builder);
+        if(builder.onTickScreen != null) builder.onTickScreen.onTick(builder, this);
+    }
+
+    //#if MC >= 12002
+    @Override
+    public boolean mouseScrolled(double mouseX, double mouseY, double scrollX, double scrollY) {
+        boolean scr = super.mouseScrolled(mouseX, mouseY, scrollX, scrollY);
+        if(mouseX <= this.builder.panelSize){
+            if (!scr && scroller_panel != null) {
+                scr = scroller_panel.mouseScrolled(mouseX, mouseY, scrollX, scrollY);
+            }
+        } else {
+            if (!scr && scroller != null) {
+                scr = scroller.mouseScrolled(mouseX, mouseY, scrollX, scrollY);
+            }
+        }
+        return scr;
+    }
+    //#elseif MC < 12002
+    //$$ @Override
+    //$$   public boolean mouseScrolled(double mouseX, double mouseY, double scrollY) {
+    //$$      boolean scr = super.mouseScrolled(mouseX, mouseY, scrollY);
+    //$$      if(mouseX <= this.builder.panelSize){
+    //$$          if (!scr && scroller_panel != null) {
+    //$$              scr = scroller_panel.mouseScrolled(mouseX, mouseY, scrollY);
+    //$$          }
+    //$$      } else {
+    //$$          if (!scr && scroller != null) {
+    //$$              scr = scroller.mouseScrolled(mouseX, mouseY, scrollY);
+    //$$          }
+    //$$      }
+    //$$      return scr;
+    //$$  }
+    //#endif
+
+    //#if MC >= 12002
+    @Override
+    public void renderBackground(GuiGraphics guiGraphics, int i, int j, float f){
+        assert this.minecraft != null;
+        super.renderBackground(guiGraphics, i, j, f);
+        guiGraphics.fill(0, 0, this.builder.panelSize, this.height, Colors.BLACK_ALPHA);
+    }
+    //#elseif MC < 12002
+    //$$
+    //$$  @Override
+    //$$  public void renderBackground(GuiGraphics guiGraphics){
+    //$$      assert this.minecraft != null;
+    //$$      super.renderBackground(guiGraphics);
+    //$$      guiGraphics.fill(0, 0, this.builder.panelSize, this.height, Colors.BLACK_ALPHA);
+    //$$  }
+    //#endif
+
+    @Override
+    public boolean keyPressed(int i, int j, int k) {
+        if(i == GLFW.GLFW_KEY_ESCAPE){
+            if(getFocused() != null && getFocused().isFocused()) {
+                getFocused().setFocused(false);
+                return true;
+            }
+        }
+        return super.keyPressed(i, j, k);
+    }
+
+    public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTicks) {
+        //#if MC < 12002
+        //$$ renderBackground(guiGraphics);
+        //#endif
+        super.render(guiGraphics, mouseX, mouseY, partialTicks);
+    }
+    public void onClose() {
+        assert this.minecraft != null;
+        this.minecraft.setScreen(builder.parent);
+    }
+}
