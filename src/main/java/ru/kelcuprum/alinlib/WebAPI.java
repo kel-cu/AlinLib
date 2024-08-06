890f4db05b97efc65792019feeadb34d0091feb9
Kel <me@kelcuprum.ru>
2024-08-06 10:06:10 +0300

    Updated files [2.0.0-alpha.5] - Rewritten WebAPI

--- src/main/java/ru/kelcuprum/alinlib/WebAPI.java
+++ src/main/java/ru/kelcuprum/alinlib/WebAPI.java
@@ -9,19 +9,52 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.Duration;
 
 public class WebAPI {
-    public static HttpClient webClient = HttpClient.newHttpClient();
-    public static String getString(String url) throws IOException, InterruptedException {
-        if(webClient == null) webClient = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
-        HttpResponse<String> response = webClient.send(request, HttpResponse.BodyHandlers.ofString());
+    public static HttpClient httpClient = HttpClient.newBuilder()
+            .version(HttpClient.Version.HTTP_2)
+            .connectTimeout(Duration.ofSeconds(10))
+            .build();
+
+    public static String getString(HttpRequest request) throws IOException, InterruptedException {
+        if (httpClient == null) {
+            httpClient = HttpClient.newBuilder()
+                    .version(HttpClient.Version.HTTP_2)
+                    .connectTimeout(Duration.ofSeconds(10))
+                    .build();
+        }
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
         return response.body();
     }
+    public static String getString(HttpRequest.Builder url) throws IOException, InterruptedException {
+        return getString(url.build());
+    }
+    public static String getString(String url) throws IOException, InterruptedException {
+        return getString(HttpRequest.newBuilder().uri(URI.create(url)).build());
+    }
+
     public static JsonObject getJsonObject(String url) throws IOException, InterruptedException {
+        return getJsonObject(HttpRequest.newBuilder().uri(URI.create(url)).header("Content-Type", "application/json").build());
+    }
+
+    public static JsonArray getJsonArray(String url) throws IOException, InterruptedException {
+        return getJsonArray(HttpRequest.newBuilder().uri(URI.create(url)).header("Content-Type", "application/json").build());
+    }
+    // HttpRequest
+    public static JsonObject getJsonObject(HttpRequest url) throws IOException, InterruptedException {
         return GsonHelper.parse(getString(url));
     }
-    public static JsonArray getJsonArray(String url) throws IOException, InterruptedException {
+
+    public static JsonArray getJsonArray(HttpRequest url) throws IOException, InterruptedException {
         return GsonHelper.parseArray(getString(url));
+    }
+    // HttpRequest.Builder
+    public static JsonObject getJsonObject(HttpRequest.Builder url) throws IOException, InterruptedException {
+        return getJsonObject(url.header("Content-Type", "application/json").build());
+    }
+
+    public static JsonArray getJsonArray(HttpRequest.Builder url) throws IOException, InterruptedException {
+        return getJsonArray(url.header("Content-Type", "application/json").build());
     }
 }
