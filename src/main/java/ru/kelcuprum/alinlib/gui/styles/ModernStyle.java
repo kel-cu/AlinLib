ebe9626836a73b542652c8806617b0a44b27a6fc
Kel <me@kelcuprum.ru>
2024-09-26 19:38:17 +0300

    Rewritten ModernStyle render [2.0.1]

--- src/main/java/ru/kelcuprum/alinlib/gui/styles/ModernStyle.java
+++ src/main/java/ru/kelcuprum/alinlib/gui/styles/ModernStyle.java
@@ -17,11 +17,11 @@ public class ModernStyle extends AbstractStyle {
         final int background = (int) (255.0F * f);
         guiGraphics.fill(x, y, x + width, y + height, background / 2 << 24);
         if (isHoveredOrFocused) {
-            guiGraphics.fill(x - 1, y - 1, x + 1 + width, y, 0xFFFFFFFF);
-            guiGraphics.fill(x - 1, y + height, x + 1 + width, y + height + 1, 0xFFFFFFFF);
+            guiGraphics.fill(x, y + 1, x + width, y, 0xFFFFFFFF);
+            guiGraphics.fill(x, y + height, x + width, y + height - 1, 0xFFFFFFFF);
 
-            guiGraphics.fill(x - 1, y, x, y + height, 0xFFFFFFFF);
-            guiGraphics.fill(x + width, y, x + 1 + width, y + height, 0xFFFFFFFF);
+            guiGraphics.fill(x, y, x+1, y + height, 0xFFFFFFFF);
+            guiGraphics.fill(x + width, y, x - 1 + width, y + height, 0xFFFFFFFF);
         }
     }
 
@@ -36,11 +36,11 @@ public class ModernStyle extends AbstractStyle {
             int yS = y + (height - 8) / 2;
             guiGraphics.fill(xS, yS, xS + 4, yS + AlinLib.MINECRAFT.font.lineHeight, Colors.CLOWNFISH);
             //
-            guiGraphics.fill(x - 1, y - 1, x + 1 + width, y, 0xFFFFFFFF);
-            guiGraphics.fill(x - 1, y + height, x + 1 + width, y + height + 1, 0xFFFFFFFF);
+            guiGraphics.fill(x, y + 1, x + width, y, 0xFFFFFFFF);
+            guiGraphics.fill(x, y + height, x + width, y + height - 1, 0xFFFFFFFF);
 
-            guiGraphics.fill(x - 1, y, x, y + height, 0xFFFFFFFF);
-            guiGraphics.fill(x + width, y, x + 1 + width, y + height, 0xFFFFFFFF);
+            guiGraphics.fill(x, y, x+1, y + height, 0xFFFFFFFF);
+            guiGraphics.fill(x + width, y, x - 1 + width, y + height, 0xFFFFFFFF);
         }
     }
 }
